dc59e29ed44a3e7e38bff1c140ad2f1c3522c593
Qianbo Yin <graysonyin717@gmail.com>
2022-12-19 22:17:25 +0000

    update mdp problems

--- mdp_problems/README.md
+++ mdp_problems/README.md
@@ -1,2 +1,15 @@
 # mdp_problems
 Different Markov Decision Process problems
+
+1. Grid MDP
+specify the grid MDP problem using gridfile.txt with the following format:
+
+gamma
+step_negative_reward
+transition_dynamics
+[grid]
+
+2. Test files
+ - gridtest.txt: easiest test file with correct solution
+ - grid_1.txt
+ - grid_maze.txt: maze like grid environment
--- mdp_problems/grid_mdp.py
+++ mdp_problems/grid_mdp.py
@@ -3,13 +3,15 @@
 # code adopted from UMich EECS 492, Fall 2019
 # Qianbo Yin
 
+# usage: python3 grid_mdp.py [file_name.txt]
+
 
 
 import sys
 import copy
 
 
-class node:
+class Node:
 	'''
 	This class is for book-keeping each state in the MDP.
 
@@ -25,7 +27,7 @@ class node:
 		self.isWall = isWall
 		self.isTerminal = isTerminal
 
-class grid:
+class Grid:
 	'''
 	This class represents our MDP problem. It consists of
 
@@ -46,11 +48,14 @@ class grid:
 		self.grid : a rectangle (defined by nrows-by-ncols) that represents 
 					the environment, with i,j-th element being a node representing the thing at i,j
 		'''
+		# read in from environment file
 		line = open(gridfile, 'r').readlines()
 		self.gamma = float(line[0])
 		self.living_cost = float(line[1])
 		mprobs = line[2].split()
 		self.mprobs = [float(n) for n in mprobs]
+
+		# read in the grid
 		self.grid = []
 		for row in line[3:]:
 			gridrow = []
@@ -59,25 +64,28 @@ class grid:
 				continue
 			for ch in row.split():
 				if ch == '*':
-					gridrow.append(node(reward=self.living_cost))
+					gridrow.append(Node(reward=self.living_cost))
 				elif ch == 'x':
-					gridrow.append(node(isWall=True))
+					gridrow.append(Node(isWall=True))
 				else:
 					try:
 						utility = float(ch)
-						gridrow.append(node(util=utility, isTerminal=True))	
+						gridrow.append(Node(util=utility, isTerminal=True))	
 					except ValueError:
 						print ("Bad grid value")
 						sys.exit()
 			self.grid.append(gridrow)
+		
+		# read in nrows, ncols
 		self.nrows = len(self.grid)
 		assert(self.nrows > 0)
 		self.ncols = len(self.grid[0])
 		assert(self.ncols > 0)
 
+
 	def printGrid(self):
 		'''
-		Print the grid
+		print out a grid with the value
 		'''
 		printstr = ''
 		for row in self.grid:
@@ -88,6 +96,12 @@ class grid:
 					printstr += 'x '
 			printstr += '\n'
 		print (printstr)
+
+	def print_optimal_policy(self):
+		'''
+		print out the optimal deterministic policy, specified by an arrow
+		'''
+		#TODO
 
 	def is_coord_open(self, i, j):
 		'''
@@ -103,6 +117,7 @@ class grid:
 
 	def doValueIteration(self, epsilon=0.00001):
 		'''
+		Value Iteration for optimal value.
 		This function modifies the utilities of each cell in the grid.
 		
 		Input:
@@ -110,44 +125,46 @@ class grid:
 			self: this MDP problem
 		
 		Output:
-			No need to return anything. You just need to modify the utility for 
-			each cell (state) in self.grid.
+			Return # of iteration completed before convergence.
+			Modify the utility for each cell (state) in self.grid.
 		'''
-		#DONE: Complete value iteration!
+		# convergence criterion
 		delta = 1
 		convergence = min((1-self.gamma)/self.gamma, 1)
 		if convergence == 0:
 			convergence = 1
+		
+		# loop until convergence, asychronous update
 		niters = 0
 		while delta >= epsilon * convergence:
 			delta = 0
 			new_grid = copy.deepcopy(self.grid)
 			for i in range(self.nrows):
 				for j in range(self.ncols):
-					cur = self.grid[i][j]
-					if cur.isWall or cur.isTerminal:
+					current = self.grid[i][j]
+					if current.isWall or current.isTerminal:
 						continue
 					# find resulting position in each direction
-					# up state
+					# up action
 					if self.is_coord_open(i-1, j):
 						up_state = self.grid[i-1][j]
 					else:
-						up_state = cur
-					# right state
+						up_state = current
+					# right action
 					if self.is_coord_open(i, j+1):
 						right_state = self.grid[i][j+1]
 					else:
-						right_state = cur
-					# down state
+						right_state = current
+					# down action
 					if self.is_coord_open(i+1, j):
 						down_state = self.grid[i+1][j]
 					else:
-						down_state = cur
-					# left state
+						down_state = current
+					# left action
 					if self.is_coord_open(i, j-1):
 						left_state = self.grid[i][j-1]
 					else:
-						left_state = cur
+						left_state = current
 					
 					# find Q-value of each action
 					north_qval = self.mprobs[0]*up_state.util + self.mprobs[1]*right_state.util + self.mprobs[2]*down_state.util + self.mprobs[3]*left_state.util
@@ -156,20 +173,44 @@ class grid:
 					west_qval =  self.mprobs[0]*left_state.util + self.mprobs[1]*up_state.util + self.mprobs[2]*right_state.util + self.mprobs[3]*down_state.util
 					
 					# bellman update for utility
-					new_grid[i][j].util = cur.reward + self.gamma * max(north_qval, south_qval, east_qval, west_qval)
+					new_grid[i][j].util = current.reward + self.gamma * max(north_qval, south_qval, east_qval, west_qval)
 					# keep track of maximum change, delta
-					change = abs(new_grid[i][j].util - cur.util)
+					change = abs(new_grid[i][j].util - current.util)
 					if change > delta:
 						delta = change
 
 			self.grid = new_grid
 			niters += 1
+		return niters
+
+	
+	def doPolicyIteration(self):
+		'''
+		Policy iteration on grid world, find optimal policy
+
+		Input:
+		nothing, self is the MDP problem
+
+		Output:
+		optimal policy, defined on each grid
+		'''
+		#TODO
+
 
 		
 			
 if __name__=='__main__':
+	# define environment from file
 	assert(len(sys.argv)>1)
-	g = grid(sys.argv[1])
-	g.doValueIteration()
+	g = Grid(sys.argv[1])
+
+	# print initial grid
+	print("Initial grid configuration:")
+	g.printGrid()
+
+	# solve MDP problem with value iteration
+	niters = g.doValueIteration()
+	print("Iteration #:", niters)
+	print("Converged state value for each grid:")
 	g.printGrid()
 
